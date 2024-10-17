fa271bce7c466949cfadddde38b8f72282b11c47
vvii <thickfive@live.com>
2024-10-17 17:55:29 +0800

    tags.c

---
+++ test/tags.c
@@ -0,0 +1,11 @@
+HEAD detached at detached-tag-20241017
+Revert currently in progress.
+  (run "git revert --continue" to continue)
+  (use "git revert --skip" to skip this patch)
+  (use "git revert --abort" to cancel the revert operation)
+
+Untracked files:
+  (use "git add <file>..." to include in what will be committed)
+        test/tags.c
+
+nothing added to commit but untracked files present (use "git add" to track)
